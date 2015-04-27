e3df892bcdbfa94ceab301a55afaebe008ff288c
Sjarlatan <benjamat@student.matnat.uio.no>
2015-04-27 17:55:00 +0200

    Dette burde funke... Men det gjør ikke det.
    Jepp.

---
+++ 3a/Oblig3a.scm
@@ -0,0 +1,51 @@
+;; Oblig3a.scm av Benjamin A. Thomas (benjamat) og Thomas Schwitalla (thoschwi)
+(load "prekode3a.scm")
+
+;;1. PROSEDYRER FOR BEDRE PROSEDYRER
+
+;;a - b)
+;;(define (mem message proc);; Den gamle.
+
+;;  (define (memoize f)
+;;  (let ((table (make-table)))
+;;  (lambda x
+;;  (let ((previously-computed-result
+;;       (lookup x table)))
+;;(or previously-computed-result
+;;  (let ((result (apply f x)))
+;;  (insert! x result table)
+;;result))))))
+
+;;(let ((original-proc proc))
+;;  (case message
+;;  ('memoize (memoize proc))
+;;('unmemoize original-proc))))) ;; Unmemoize funker ikke.
+  
+(define (mem message proc)
+  
+  (define (memoize f)
+    (let ((table (make-table))
+          (original-proc proc))
+          (lambda x
+            (case message 
+              ('memoize 
+               (let ((previously-computed-result
+                      (lookup x table)))
+                 (or previously-computed-result
+                     (let ((result (apply f x)))
+                       (insert! x result table)
+                       (insert! original-proc "original" table)
+                       result))))
+               ('umemoize
+                (lookup "original" table))))))
+            
+            (memoize proc))
+
+(set! fib (mem 'memoize fib))
+(fib 3)
+(fib 3)
+(fib 2)
+(fib 4)
+(set! fib (mem 'unmemoize fib))
+(fib 3)
+
